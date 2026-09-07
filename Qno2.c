40a31c9548ca7868058c7d2f11fe847ecaff7687
aadarsha-2064 <aadarshghimire456@gmail.com>
2026-09-07 19:33:54 +0545

    update in code

--- Qno2.c
+++ Qno2.c
@@ -4,38 +4,76 @@
 
 int main()
 {
-    int n;
+    int n, na, nb;
     int i;
 
-    printf("Enter size of sets: ");
+    printf("Enter size of Universal Set: ");
     scanf("%d", &n);
 
+    int U[n];
     int A[n];
     int B[n];
 
-    printf("Enter Boolean A: ");
+    int a = 0;
+    int b = 0;
+
+    printf("Enter Universal Set U: ");
     for(i = 0; i < n; i++)
+        scanf("%d", &U[i]);
+
+    printf("Enter size of Set A: ");
+    scanf("%d", &na);
+
+    printf("Enter Set A: ");
+    for(i = 0; i < na; i++)
         scanf("%d", &A[i]);
 
-    printf("Enter Boolean B: ");
-    for(i = 0; i < n; i++)
+    printf("Enter size of Set B: ");
+    scanf("%d", &nb);
+
+    printf("Enter Set B: ");
+    for(i = 0; i < nb; i++)
         scanf("%d", &B[i]);
 
-    printf("Union: ");
-    for(i = 0; i < n; i++)
-        printf("%d ", A[i] | B[i]);
+    // Convert A into bit representation
+    for(i = 0; i < na; i++)
+        a = a | (1 << A[i]);
 
+    // Convert B into bit representation
+    for(i = 0; i < nb; i++)
+        b = b | (1 << B[i]);
+
+    // Union
+    printf("\nUnion: ");
+    for(i = 0; i < n; i++)
+    {
+        if((a | b) & (1 << i))
+            printf("%d ", U[i]);
+    }
+
+    // Intersection
     printf("\nIntersection: ");
     for(i = 0; i < n; i++)
-        printf("%d ", A[i] & B[i]);
+    {
+        if((a & b) & (1 << i))
+            printf("%d ", U[i]);
+    }
 
+    // Difference A-B
     printf("\nDifference A-B: ");
     for(i = 0; i < n; i++)
-        printf("%d ", A[i] & ~B[i]);
+    {
+        if((a & ~b) & (1 << i))
+            printf("%d ", U[i]);
+    }
 
+    // Complement of A
     printf("\nComplement A: ");
     for(i = 0; i < n; i++)
-        printf("%d ", ~A[i] & 1);
+    {
+        if((~a) & (1 << i))
+            printf("%d ", U[i]);
+    }
 
     return 0;
 }
--- Qno4.c
+++ Qno4.c
@@ -1,55 +1,127 @@
-// Question: Calculate the cardinality of the union of three finite sets using the Inclusion-Exclusion formula.
+// Question: Calculate the cardinality of the union of three finite sets
+// using the Inclusion-Exclusion formula.
 
 #include <stdio.h>
 
 int main()
 {
-    int n;
-    int i;
+    int n, na, nb, nc;
+    int i, j;
+
     int a = 0, b = 0, c = 0;
     int ab = 0, ac = 0, bc = 0, abc = 0;
 
-    printf("Enter size of sets: ");
+    printf("Enter size of Universal Set: ");
     scanf("%d", &n);
 
-    int A[n];
-    int B[n];
-    int C[n];
+    int U[n];
 
-    printf("Enter A: ");
+    printf("Enter Universal Set U: ");
     for(i = 0; i < n; i++)
+        scanf("%d", &U[i]);
+
+    printf("Enter size of Set A: ");
+    scanf("%d", &na);
+
+    int A[na];
+
+    printf("Enter Set A: ");
+    for(i = 0; i < na; i++)
         scanf("%d", &A[i]);
 
-    printf("Enter B: ");
-    for(i = 0; i < n; i++)
+    printf("Enter size of Set B: ");
+    scanf("%d", &nb);
+
+    int B[nb];
+
+    printf("Enter Set B: ");
+    for(i = 0; i < nb; i++)
         scanf("%d", &B[i]);
 
-    printf("Enter C: ");
-    for(i = 0; i < n; i++)
+    printf("Enter size of Set C: ");
+    scanf("%d", &nc);
+
+    int C[nc];
+
+    printf("Enter Set C: ");
+    for(i = 0; i < nc; i++)
         scanf("%d", &C[i]);
 
-    for(i = 0; i < n; i++)
+    // Cardinality of each set
+    a = na;
+    b = nb;
+    c = nc;
+
+    // Find A intersection B
+    for(i = 0; i < na; i++)
     {
-        a += A[i];
-        b += B[i];
-        c += C[i];
-
-        if(A[i] & B[i])
-            ab++;
-
-        if(A[i] & C[i])
-            ac++;
-
-        if(B[i] & C[i])
-            bc++;
-
-        if(A[i] & B[i] & C[i])
-            abc++;
+        for(j = 0; j < nb; j++)
+        {
+            if(A[i] == B[j])
+            {
+                ab++;
+                break;
+            }
+        }
     }
 
-    printf("|A| = %d\n", a);
+    // Find A intersection C
+    for(i = 0; i < na; i++)
+    {
+        for(j = 0; j < nc; j++)
+        {
+            if(A[i] == C[j])
+            {
+                ac++;
+                break;
+            }
+        }
+    }
+
+    // Find B intersection C
+    for(i = 0; i < nb; i++)
+    {
+        for(j = 0; j < nc; j++)
+        {
+            if(B[i] == C[j])
+            {
+                bc++;
+                break;
+            }
+        }
+    }
+
+    // Find A intersection B intersection C
+    for(i = 0; i < na; i++)
+    {
+        for(j = 0; j < nb; j++)
+        {
+            if(A[i] == B[j])
+            {
+                int k;
+
+                for(k = 0; k < nc; k++)
+                {
+                    if(A[i] == C[k])
+                    {
+                        abc++;
+                        break;
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+
+    printf("\n|A| = %d\n", a);
     printf("|B| = %d\n", b);
     printf("|C| = %d\n", c);
+
+    printf("|A intersection B| = %d\n", ab);
+    printf("|A intersection C| = %d\n", ac);
+    printf("|B intersection C| = %d\n", bc);
+    printf("|A intersection B intersection C| = %d\n", abc);
 
     printf("|A U B U C| = %d",
            a + b + c - ab - ac - bc + abc);
